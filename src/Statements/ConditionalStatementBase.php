663e709bd503818c4eba63f5de5d4bf6f3ff56a5
Jarzon <j@jasonvaillancourt.ca>
2019-02-06 17:51:06 -0500

    Remove string type hint from column in conditions Fix subcondition

--- src/Statements/ConditionalStatementBase.php
+++ src/Statements/ConditionalStatementBase.php
@@ -11,20 +11,22 @@ abstract class ConditionalStatementBase extends StatementBase
 
     public function where($column, ?string $operator = null, $value = null, $isRaw = false): self
     {
-        $value = $this->param($value, $column, $isRaw);
-
         $conditionsCount = count($this->conditions);
         if($conditionsCount > 0 && $this->conditions[$conditionsCount-1] != '(') {
             $this->addCondition('AND');
         }
 
-        if(is_callable($column) && $column instanceof \Closure) {
+        if($operator === null && is_callable($column) && $column instanceof \Closure) {
             $this->addCondition('(');
             $column($this);
             $this->addCondition(')');
-        } else {
-            $this->addCondition(new Condition($column, $operator, $value));
+
+            return $this;
         }
+
+        $value = $this->param($value, $column, $isRaw);
+
+        $this->addCondition(new Condition($column, $operator, $value));
 
         return $this;
     }
@@ -36,53 +38,53 @@ abstract class ConditionalStatementBase extends StatementBase
         return $this;
     }
 
-    public function or(string $column, string $operator, $value, $isRaw = false): self
+    public function or($column, string $operator, $value, $isRaw = false): self
     {
         $this->addCondition('OR');
 
-        $this->addCondition(new Condition($this->$column, $operator, $this->param($value, $column)));
+        $this->addCondition(new Condition($column, $operator, $this->param($value, $column)));
 
         return $this;
     }
 
-    public function between(string $column, $start, $end, $isRaw = false): self
+    public function between($column, $start, $end, $isRaw = false): self
     {
-        $this->addCondition(new BetweenCondition($this->$column, $this->param($start, "{$column}1"), $this->param($end, "{$column}2")));
+        $this->addCondition(new BetweenCondition($column, $this->param($start, "{$column}1"), $this->param($end, "{$column}2")));
 
         return $this;
     }
 
-    public function notBetween(string $column, $start, $end, $isRaw = false): self
+    public function notBetween($column, $start, $end, $isRaw = false): self
     {
-        $this->addCondition(new BetweenCondition($this->$column, $this->param($start, "{$column}1"), $this->param($end, "{$column}2"), true));
+        $this->addCondition(new BetweenCondition($column, $this->param($start, "{$column}1"), $this->param($end, "{$column}2"), true));
 
         return $this;
     }
 
-    public function in(string $column, array $list, $isRaw = false): self
+    public function in($column, array $list, $isRaw = false): self
     {
-        $this->addCondition(new InCondition($this->$column, $list));
+        $this->addCondition(new InCondition($column, $list));
 
         return $this;
     }
 
-    public function notIn(string $column, array $list, $isRaw = false): self
+    public function notIn($column, array $list, $isRaw = false): self
     {
-        $this->addCondition(new InCondition($this->$column, $list, true));
+        $this->addCondition(new InCondition($column, $list, true));
 
         return $this;
     }
 
-    public function isNull(string $column, $isRaw = false): self
+    public function isNull($column, $isRaw = false): self
     {
-        $this->addCondition(new Condition($this->$column, 'IS', null));
+        $this->addCondition(new Condition($column, 'IS', null));
 
         return $this;
     }
 
-    public function isNotNull(string $column, $isRaw = false): self
+    public function isNotNull($column, $isRaw = false): self
     {
-        $this->addCondition(new Condition($this->$column, 'IS NOT', null));
+        $this->addCondition(new Condition($column, 'IS NOT', null));
 
         return $this;
     }
