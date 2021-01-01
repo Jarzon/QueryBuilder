6464a790c7609fca99e71a69b828970e258f29f3
jarzon <j@jasonvaillancourt.ca>
2020-12-31 23:38:00 -0500

    Replace date_add by arithmetic version

--- src/Columns/Date.php
+++ src/Columns/Date.php
@@ -16,7 +16,14 @@ class Date extends ColumnBase
 
     public function dateAdd(string $intervalAddition)
     {
-        $this->output .= new Raw("DATE_ADD({$this->getOutput()}, INTERVAL $intervalAddition)");
+        $this->output .= new Raw("{$this->getOutput()} + INTERVAL $intervalAddition");
+
+        return $this;
+    }
+
+    public function dateSub(string $intervalAddition)
+    {
+        $this->output .= new Raw("{$this->getOutput()} - INTERVAL $intervalAddition");
 
         return $this;
     }
